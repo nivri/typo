75d95fab1414bf39646f4b730665027adb761221
nivri <erwin.klosko@gmail.com>
2016-02-09 18:38:15 +0000

    Legacy HW-2 (new feature) - checking solution.

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -150,8 +150,8 @@ class Admin::ContentController < Admin::BaseController
     if  params[:article] and params[:article][:merge] 
       if params[:merge_with] and params[:merge_with] != '' and params[:merge_with] != id
         
-        #@article.merge_with(params[:merge_with])
-        Article.merge(id, params[:merge_with].to_i)
+        @article.merge_with(params[:merge_with])
+        # Article.merge(id, params[:merge_with].to_i)
         
         # article = Article.get_or_build_article(params[:merge_with])
         # @article.body += article.body
